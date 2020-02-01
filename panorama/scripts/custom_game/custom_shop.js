b645ef9579e3c6f0caad1d6b0aecadf9b8c49503
Unknown <superdotaplaya@gmail.com>
2020-02-01 14:16:19 -0500

    Shop should be working now! Beta tester particle is unpurchaseable and I will add them manually (others are purchaseable)

--- panorama/scripts/custom_game/custom_shop.js
+++ panorama/scripts/custom_game/custom_shop.js
@@ -22,16 +22,22 @@ function PlayerSelectBetaTester()
 {
 	player = Game.GetLocalPlayerInfo().player_id
 $.Msg("Player selected beta tester particle in shop!")
-GameEvents.SendCustomGameEventToServer( "player_purchase_custom_item", {item: "beta_tester", price: 200, player_id: player} )
+GameEvents.SendCustomGameEventToServer( "player_purchase_custom_item", {item: "beta_tester", price: 100000000000000000000000000000000000000000, player_id: player} )
 }
 
 function LoadCurrency(Data)
 {
-	$.Msg(Data.beta_tester_particle)
-	if (Data.beta_tester_particle == "true") {
-		$("#beta_particle_owned_text").text = "(Owned)"
-	}
+	
 $("#player-currency").text = Data.player_currency + " Gems"
 }
 
 var handle = GameEvents.Subscribe( "player_currency_loaded", LoadCurrency );
+var handle = GameEvents.Subscribe( "player_particles_loaded", LoadParticles );
+
+function LoadParticles(Data)
+{
+$.Msg(Data.beta_tester_particle)
+	if (Data.beta_tester_particle == "true") {
+		$("#beta_particle_owned_text").text = "(Owned)"
+	}
+}
