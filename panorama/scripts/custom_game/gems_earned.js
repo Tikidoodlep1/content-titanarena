f5d90ac96667de6035159a8dafe923594ea5e63b
Unknown <superdotaplaya@gmail.com>
2020-02-05 17:10:50 -0500

    end game scoreboard now correctly displays player gems earned

--- panorama/scripts/custom_game/gems_earned.js
+++ panorama/scripts/custom_game/gems_earned.js
@@ -1,6 +1,8 @@
 function GetGemsEarned()
 {
-	
-
-	
+for (i = 0; i < 10; i++) {
+  	var kills = Players.GetKills(i);
+  	var assists = Players.GetAssists(i)
+  	$("#PlayerGemsEarned").text = kills + (assists/2)
+}
 }
